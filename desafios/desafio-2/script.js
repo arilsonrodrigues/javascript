93eb3901e3289549c22c0fa9e08d46069d14d307
Arilson Magalhães Rodrigues <arilson.magalhaes00@gmail.com>
2024-09-11 22:34:43 -0300

    Atualizei

--- desafios/desafio-2/script.js
+++ desafios/desafio-2/script.js
@@ -1,38 +1,38 @@
 function info() {
-    data = new Date()
-    ano = data.getFullYear()
-    txtano = document.getElementById('txtano')
-    res = document.getElementById('res')
-    if (txtano.value == 0 || txtano.value > ano || txtano.value < 1910){
+   data = new Date()
+   ano = data.getFullYear()
+   txtn = document.getElementById('txtn')
+   res = document.getElementById('res')
+   if (txtn.value == 0 || txtn.value > ano || txtn.value < 1910){
         alert('[ERRO] digite novamente')
-    } else{
-        sex = document.getElementsByName('rdxs')
-        idade = ano - Number(txtano.value)
-        img = document.getElementById('imagem')
+   }else{
+        sex = document.getElementsByName('radsex')
+        idade = ano - Number(txtn.value)
+        imagem = document.getElementById('img')
         genero = ''
-        if (sex[0].checked){
-            genero ='Homem'
-            if (idade > 0 && idade < 14){
-                img.src = 'crianca-m.jpg'
-            } else if(idade < 21){
-                img.src = 'jovem-m.jpg'
-            } else if(idade < 60){
-                img.src = 'adulto-m.jpg'
-            } else {
-                img.src = 'idoso-m.jpg'
+        if(sex[0].checked){
+            genero = 'Homem'
+            if(idade > 0 && idade < 14){
+                img.src = `crianca-m.jpg`
+            }else if(idade < 21){
+                img.src = `jovem-m.jpg`
+            }else if(idade < 60){
+                img.src = `adulto-m.jpg`
+            }else{
+                img.src = `idoso-m.jpg`
             }
-        }else {
+        }else{
             genero = 'Mulher'
-            if (idade > 0 && idade < 14){
-                img.src = 'crianca-f.jpg'
-            } else if(idade < 21){
-                img.src = 'jovem-f.jpg'
-            } else if(idade < 60){
-                img.src = 'adulta-f.jpg'
-            } else {
-                img.src = 'idosa-f.jpg'
+            if(idade > 0 && idade < 14){
+                img.src = `crianca-f.jpg`
+            }else if(idade < 21){
+                img.src = `jovem-f.jpg`
+            }else if(idade < 60){
+                img.src = `adulta-f.jpg`
+            }else{
+                img.src = `idosa-f.jpg`
             }
         }
-        res.innerHTML = `${genero} e tem ${idade} anos`
-    }
+        res.innerHTML = `${genero} com ${idade} anos`
+   }
 }
